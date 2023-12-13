7055db3dceac8d089d74bc64c537927d04d5b5ce
Fulup Ar Foll <fulup@iot.bzh>
2023-12-13 12:34:03 +0100

    Added rpath search

--- slac-lib/capi/build.rs
+++ slac-lib/capi/build.rs
@@ -6,12 +6,17 @@
  * License: $RP_BEGIN_LICENSE$ SPDX:MIT https://opensource.org/licenses/MIT $RP_END_LICENSE$
  *
 */
-extern crate bindgen;
+use std::env;
 
 fn main() {
     // invalidate the built crate whenever the wrapper changes
     println!("cargo:rerun-if-changed=capi/capi-map.h");
-
+    println!("cargo:rustc-link-search=/usr/local/lib64");
+    if let Ok(value) = env::var("CARGO_TARGET_DIR") {
+        if let Ok(profile) = env::var("PROFILE") {
+            println!("cargo:rustc-link-search=crate={}{}", value, profile);
+        }
+    }
     let header = "
     // -----------------------------------------------------------------------
     //         <- private 'libslac' Rust/C unsafe binding ->
@@ -25,7 +30,7 @@ fn main() {
     let libslac = bindgen::Builder::default()
         .header("capi/capi-map.h") // Pionix C++ prototype wrapper input
         .raw_line(header)
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .derive_debug(false)
         .layout_tests(false)
         //.allowlist_function("read")
